ae1b4475d84d42f29c2d4432c17f998e5e9e38ab
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-09-05 23:42:07 +0300

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Mykola Chaikovskyi on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
